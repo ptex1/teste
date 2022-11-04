7a84df3d2dbaf360b8dd3476496d7b5e3281e5c7
bia-fontenelle <beatrizaf1@al.insper.edu.br>
2022-11-04 14:27:40 -0300

    codigo principal

--- EP2/codigo_principal.py
+++ EP2/codigo_principal.py
@@ -1,6 +1,13 @@
 from base_de_perguntas import *
 from funcoes_obrigatorias import *
 
+#checagem da base de perguntas
+
+validacao =  valida_questoes(quest)
+base_transformada = transforma_base(quest)
+print(validacao)
+
+
 #introducao
 
 print("\nOlá! Você está na Fortuna DesSoft e terá a oportunidade de enriquecer!\n\n")
@@ -11,5 +18,7 @@ input("Aperte ENTER para continuar...")
 print("\nO jogo já vai começar! Lá vem a primeira questão!\n\n")
 print("Vamos começar com questões de nível FACIL!")
 input("Aperte ENTER para continuar...")
+
+
 
 
