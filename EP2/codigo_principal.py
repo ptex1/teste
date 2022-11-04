574715eb8eac8e76f753178fa81037d34b174311
bia-fontenelle <beatrizaf1@al.insper.edu.br>
2022-11-04 09:27:59 -0300

    Inicio do código principal + introdução

---
+++ EP2/codigo_principal.py
@@ -0,0 +1,15 @@
+from base_de_perguntas import *
+from funcoes_obrigatorias import *
+
+#introducao
+
+print("\nOlá! Você está na Fortuna DesSoft e terá a oportunidade de enriquecer!\n\n")
+nome = input("Qual seu nome? ")
+print(f"Ok {nome}, você tem direito a pular 3 vezes e 2 ajudas!\n")
+print("As opcões de resposta são 'A','B','C','D','ajuda','pula' e 'parar'!\n\n")
+input("Aperte ENTER para continuar...")
+print("\nO jogo já vai começar! Lá vem a primeira questão!\n\n")
+print("Vamos começar com questões de nível FACIL!")
+input("Aperte ENTER para continuar...")
+
+
