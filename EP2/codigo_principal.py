dd72e91580ca7c85588885c5116b2b75c788b9c7
bia-fontenelle <beatrizaf1@al.insper.edu.br>
2022-11-06 18:29:50 -0300

    inicio while loop

--- EP2/codigo_principal.py
+++ EP2/codigo_principal.py
@@ -1,6 +1,15 @@
 from base_de_perguntas import *
 from funcoes_obrigatorias import *
 
+
+def pergunta_ao_usuario():
+    resposta = input("Qual sua resposta?! ")
+    if resposta in ["A", "B", "C", "D", "ajuda", "pular"]:
+        return resposta
+    else:
+        print("Resposta inválida")
+        pergunta_ao_usuario()
+
 #checagem da base de perguntas
 
 jogo_defeito = False
@@ -29,10 +38,45 @@ input("Aperte ENTER para continuar...")
 
 base_transformada = transforma_base(quest)
 premio = 0
-num_pergunta = 1
+premios = [1000, 5000, 10000, 30000, 50000, 100000, 300000, 500000, 1000000]
+continuar = True
+num_questao = 1
 lista_questoes_sorteadas = []
-while premio < 1000000:
-    sorteia_questao_inedida(base_transformada,"FACIL",lista_questoes_sorteadas)
+num_ajudas = 2
+num_pulos = 3
+
+while premio < 1000000 and continuar:
+    pode_ajuda = True
+    if num_questao < 4:
+        nivel = "facil"
+    elif num_questao < 7:
+        nivel = "medio"
+    else:
+        nivel = "dificil"
+    questao = sorteia_questao_inedida(base_transformada,nivel,lista_questoes_sorteadas)
+    print(questao_para_texto(questao,num_questao))
+    resposta = pergunta_ao_usuario()
+    
+    if resposta == "ajuda":
+        if pode_ajuda:
+            if num_ajudas > 0:
+                num_ajudas -= 1
+                pode_ajuda = False
+                print(f"Ok, lá vem ajuda! Você ainda tem {num_ajudas} ajudas!")
+                input("Aperte ENTER para continuar...")
+                gera_ajuda(questao)
+                print(questao_para_texto(questao,num_questao))
+            else:
+                print("Você não possui mais ajudas")
+                resposta = pergunta_ao_usuario()
+        else:
+            print("Você já pediu ajuda nesta pergunta, escolha outra opção")
+    if resposta == "pular":
+        print()
+    num_questao += 1
+
+
+
 
 
 
